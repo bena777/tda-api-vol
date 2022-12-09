d42e2e2a59cfbd5d22ee42c430ed02fcb48b2e6c
ben adelman <109058028+bena777@users.noreply.github.com>
2022-12-09 02:19:02 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key = 'YOURCODE@AMER.OAUTHAP' #find code on TD ameritrade dev site 
+token_path = 'token'
+redirect_uri = 'https://localhost'
+chromedriver_path= 'chromedriver' #paste path to chromedriver here 
