0b44767d17e632aeb923a6b1a6abc457a889de77
Ben <bowserben@icloud.com>
2023-01-16 00:18:41 -0500

    added iv_screener.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_key = 'K5XWGTT7RXB3POETTQLZSQALSOUBBDU0@AMER.OAUTHAP' #find code on TD ameritrade dev site
+api_key = 'YOURKEY@AMER.OAUTHAP' #find code on TD ameritrade dev site
 token_path = 'token'
 redirect_uri = 'https://localhost'
 chromedriver_path= 'chromedriver' #paste path to chromedriver here
