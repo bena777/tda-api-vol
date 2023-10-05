a923bf8d46e49905a138d52a92e00797f7cd9cac
ben <109058028+bena777@users.noreply.github.com>
2023-10-04 21:40:01 -0400

    fixed and added helper function

--- tda-vol/scripts/ivx.py
+++ tda-vol/scripts/ivx.py
@@ -1,14 +1,23 @@
+import pandas as pd
 from main import tda_auth
 from datetime import datetime
+import numpy as np
+
+
+def linear_interp_list(y):
+    df = pd.Series(y)
+    df = df.replace("NaN",np.nan)
+    df = df.interpolate(limit_area='inside')
+    return list(df.values)
+
 def ivx_calculation(ticker,front):
     """
     :param ticker:
     :param front month expiration date (Must be over 7DTE):
-    :param back month expiration date:
     Calculates the IVx of inputted ticker in TastyTrade style way
-    IVx serves a similar function as the VIX index except it is only based on ATM/ near ATM optiopns leading to it being able to be done on ANY optionable security
-    Weights ATM options at 60%, +-1 at 30%, and +-2 at 10%
-    This is done for both options, and they are then simply averaged to return the IVX
+    IVx serves a similar function as the VIX index except it can be calculated on any underlying
+    However, it works better on liquid assets as all NaN values are linearly interpolated
+    Weights ATM options at 0.4 and cuts each +-i in half each time (weights subject to change)
     Created by Ben A
     """
     c = tda_auth()
@@ -24,16 +33,20 @@ def ivx_calculation(ticker,front):
             dif=abs(float(x)-price)
             atm_option=x
     front_chain_c=r.json()['callExpDateMap'][str(front)+":"+str(dte_front)]
-    front_chain_p=r.json()['putExpDateMap'][str(front)+":"+str(dte_front)]
     ivx_front=0
-    vols_c = [i[0]['volatility'] for i in list(front_chain_c.values())]
+    vols = [i[0]['volatility'] for i in list(front_chain_c.values())]
     strikes = list(front_chain_c.keys())
-    vols_p = [i[0]['volatility'] for i in list(front_chain_p.values())]
 
     atm_index = strikes.index(str(float(atm_option)))
+    starting_factor = 0.3 #factor for splitting weights (should be 0.3)
+    factor = starting_factor/2
+    sum = starting_factor+0.1 #+0.1 is used to make it equal 1 as other
+    ivx_front += vols[atm_index]*(starting_factor+0.1)
 
-    ivx_front += (vols_c[atm_index]*0.6 + vols_c[atm_index+1]*0.3 + vols_c[atm_index-1]*0.1) #actually works and is more efficient but now need to work on actual methodlogy
-    ivx_front += (vols_p[atm_index]*0.6 + vols_p[atm_index+1]*0.3 + vols_p[atm_index-1]*0.1)
-
-    ivx=(ivx_front/2)
+    vols = linear_interp_list(vols) #interpolate NaN values
+    for i in range(1,atm_index): #can adjust to accomidate for more or less options
+        sum+=factor*2
+        ivx_front += ((vols[atm_index+i]*factor) + (vols[atm_index-i]*factor)) #actually works and is more efficient but now need to work on actual methodlogy
+        factor = factor / 2
+    ivx=ivx_front
     return ivx
